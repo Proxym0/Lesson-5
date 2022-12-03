4c815ed71c6441013a48ec5d7d68370429ddb213
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-04 00:27:41 +0300

    save1

--- src/group1/Main.java
+++ src/group1/Main.java
@@ -1,5 +1,23 @@
+package group1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int coef = scanner.nextInt();
+        int[][][] array = new int[][][]{
+                {{12, 11}, {10, 9}, {8, 7}},
+                {{6, 5}, {4, 3}, {2, 1}}
+                };
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int c = 0; c < array[i][j].length; c++){
+                    array[i][j][c]+=coef;
+
+                }
+            }
+        }System.out.print(Arrays.deepToString(array));
     }
 }
