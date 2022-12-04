02b9b2c37a00f875f78192de0829386370114255
Proxym0 <vladi.shapovalov@gmail.com>
2022-12-04 20:32:27 +0300

    save6

--- src/group2/Class3.java
+++ src/group2/Class3.java
@@ -5,14 +5,14 @@ public class Class3 {
         int[][] array = {{1, 2, 3},
                          {5, 6, 7},
                          {9, 10, 11}};
-        for (int i = array.length - 1; i >= 0; i--) {
+        for (int i = 0 ; i < array.length ; i++) {
             for (int j = 0; j < array[i].length; j++) {
             }System.out.print(array[i][i] + " ");
             }
             System.out.println(" ");
-            for (int i = array.length - 1; i >= 0; i--) {
+            for (int i = 0 ; i < array.length ; i++) {
                 for (int j = 0; j < array[i].length; j++) {
-            }System.out.print(array[i][array.length - 1 - i] + " ");
+            }System.out.print(array[i][array.length -1 - i] + " ");
         }
     }
 }
